405b27247b27ad06c82671588a4bcd9f522114e5
Sengar <amol-rajendrasingh.sengar@capgemini.com>
2022-03-16 16:20:33 +0530

    Initial commit

---
+++ src/main/java/com/cg/MHealthApplication.java
@@ -0,0 +1,15 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MHealthApplication.class, args);
+		System.out.println("Hello");
+		System.out.println("Hello1");
+	}
+
+}
