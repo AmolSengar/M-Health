ad5670705f9a3f94ab5532f816856dcd0c66f7ee
Sengar <amol-rajendrasingh.sengar@capgemini.com>
2022-03-17 13:28:38 +0530

    next

--- src/main/java/com/cg/dao/DoctorJPARepository.java
+++ src/main/java/com/cg/dao/DoctorJPARepository.java
@@ -2,9 +2,7 @@ package com.cg.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.cg.model.Doctor;
-import com.cg.model.Users;
 
 @Repository
 public interface DoctorJPARepository extends JpaRepository<Doctor, Integer> {
---
+++ src/main/java/com/cg/restTemplate.java
@@ -0,0 +1,15 @@
+package com.cg;
+
+import org.springframework.http.ResponseEntity;
+
+import com.cg.model.Doctor;
+
+public enum restTemplate {
+	getForEntity();
+
+	ResponseEntity<Doctor> getForEntity(String string, Class<Doctor> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
